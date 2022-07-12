baa1af7af85ca9a8875b54676b96bb64a7883817
Karetskas_Evgeny <kub99999@yandex.ru>
2022-07-12 18:20:05 +0500

    Created "RangeTask" project.
    1. Created "Range" class.
    2. Created "UsingRangeClass" class.

---
+++ Tasks/RangeTask/Range.cs
@@ -0,0 +1,26 @@
+﻿namespace Academits.Karetskas
+{
+    public sealed class Range
+    {
+        public double From { get; set; }
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            const double epsilon = 1.0e-10;
+
+            return number - From >= -epsilon && To - number >= epsilon;
+        }
+    }
+}
---
+++ Tasks/RangeTask/UsingRangeClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Academits.Karetskas
+{
+    public static class UsingRangeClass
+    {
+        static void Main()
+        {
+            Console.WriteLine("The program for working with \"Range\" class.");
+            Console.WriteLine();
+
+            Console.Write("Please, enter the first real number: ");
+            double from = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please, enter the second real number: ");
+            double to = Convert.ToDouble(Console.ReadLine());
+
+            const double epsilon = 1.0e-10;
+
+            if (from - to > epsilon)
+            {
+                double temp = to;
+                to = from;
+                from = temp;
+            }
+
+            Range range = new Range(from, to);
+
+            Console.Write("Please, enter the real number: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("The range has a size {0}. ", range.GetLength());
+
+            if (range.IsInside(number))
+            {
+                Console.WriteLine("Number {0} is part of range.", number);
+
+                return;
+            }
+
+            Console.WriteLine("Number {0} isn't part of range.", number);
+        }
+    }
+}
