635649e20c03ca3bbffabcf29775868dccc94443
Karetskas_Evgeny <kub99999@yandex.ru>
2022-09-16 21:25:07 +0500

    Created "ArrayListTask" project.

---
+++ Tasks/ArrayListTask/ArrayList.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Academits.Karetskas.ArrayListTask
+{
+    public sealed class ArrayList<T> : IList<T>
+    {
+        private T[] items;
+        private int length;
+        private int modCount;
+
+        public int Count => length;
+
+        public int Capacity
+        {
+            get => items.Length;
+
+            set
+            {
+                if (value < length)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), $"The \"{nameof(value)}\" argument out of range of array. " +
+                        $"Valid range from {length} to {int.MaxValue}.");
+                }
+
+                if (value == items.Length)
+                {
+                    return;
+                }
+
+                T[] oldArray = items;
+
+                items = new T[value];
+
+                Array.Copy(oldArray, 0, items, 0, length);
+
+                modCount++;
+            }
+        }
+
+        public bool IsReadOnly => false;
+
+        public T this[int index]
+        {
+            get
+            {
+                if (length == 0)
+                {
+                    throw new InvalidOperationException("There are no items in the list.");
+                }
+
+                if (index < 0 || index >= length)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), $"The \"{nameof(index)}\" argument out of range of array. " +
+                        $"Valid range from 0 to {length - 1}.");
+                }
+
+                return items[index];
+            }
+
+            set
+            {
+                if (length == 0)
+                {
+                    throw new InvalidOperationException("There are no items in the list.");
+                }
+
+                if (index < 0 || index >= length)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), $"The \"{nameof(index)}\" argument out of range of array. " +
+                        $"Valid range from 0 to {length - 1}.");
+                }
+
+                items[index] = value;
+
+                modCount++;
+            }
+        }
+
+        public ArrayList(int capacity)
+        {
+            if (capacity < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(capacity), $"The \"{nameof(capacity)}\" argument out of range of array. " +
+                    $"Valid range from 0 to {int.MaxValue}.");
+            }
+
+            items = new T[capacity];
+        }
+
+        private void IncreaseCapacity()
+        {
+            if (items.Length == 0)
+            {
+                items = new T[1];
+
+                return;
+            }
+
+            T[] oldArray = items;
+
+            items = length >= int.MaxValue / 2 ? new T[int.MaxValue]
+                                               : new T[length * 2];
+
+            Array.Copy(oldArray, 0, items, 0, length);
+        }
+
+        public int IndexOf(T item)
+        {
+            return Array.IndexOf(items, item, 0, length);
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index < 0 || index > length || length == int.MaxValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), $"The \"{nameof(index)}\" argument out of range of array. " +
+                    $"Valid range from 0 to {length}.");
+            }
+
+            if (index == items.Length)
+            {
+                Add(item);
+
+                return;
+            }
+
+            if (length + 1 > items.Length)
+            {
+                IncreaseCapacity();
+            }
+
+            Array.Copy(items, index, items, index + 1, length - index);
+
+            items[index] = item;
+
+            length++;
+            modCount++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (length == 0)
+            {
+                throw new InvalidOperationException("There are no items in the list.");
+            }
+
+            if (index < 0 || index >= length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), $"The \"{nameof(index)}\" argument out of range of array. " +
+                    $"Valid range from 0 to {length - 1}.");
+            }
+
+            Array.Copy(items, index + 1, items, index, length - (index + 1));
+
+            length--;
+            modCount++;
+        }
+
+        public void Add(T item)
+        {
+            if (length == int.MaxValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length), $"The \"{nameof(length)}\" argument out of range of array. " +
+                    $"Valid range from 0 to {length}.");
+            }
+
+            if (length == items.Length)
+            {
+                IncreaseCapacity();
+            }
+
+            items[length] = item;
+
+            length++;
+            modCount++;
+        }
+
+        public void Clear()
+        {
+            length = 0;
+            modCount++;
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (Equals(item, items[i]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array), $"Argument \"{nameof(array)}\" is null.");
+            }
+
+            if (length == 0)
+            {
+                throw new InvalidOperationException("There are no items in the list.");
+            }
+
+            if (arrayIndex < 0 || arrayIndex >= length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex), $"The \"{nameof(arrayIndex)}\" argument out of range of array. " +
+                    $"Valid range from 0 to {length - 1}.");
+            }
+
+            Array.Copy(items, arrayIndex, array, 0, length - arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            int itemIndex = IndexOf(item);
+
+            if (itemIndex == -1)
+            {
+                return false;
+            }
+
+            RemoveAt(itemIndex);
+
+            return true;
+        }
+
+        public void TrimExcess()
+        {
+            const double epsilon = 1.0e-10;
+
+            double listFullness = (double)length / items.Length * 100;
+
+            if (90 - listFullness > -epsilon)
+            {
+                Array.Resize(ref items, length);
+            }
+        }
+
+        public override string ToString()
+        {
+            if (length == 0)
+            {
+                return "[]";
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append('[');
+
+            foreach (T item in this)
+            {
+                if (item is null)
+                {
+                    stringBuilder.Append("null")
+                        .Append(", ");
+
+                    continue;
+                }
+
+                stringBuilder.Append(item.ToString())
+                    .Append(", ");
+            }
+
+            stringBuilder.Remove(stringBuilder.Length - 2, 2)
+                .Append(']');
+
+            return stringBuilder.ToString();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            int currentModCount = modCount;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (currentModCount != modCount)
+                {
+                    throw new InvalidOperationException("The list has been modified during the iteration.");
+                }
+
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Tasks/ArrayListTask/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+namespace Academits.Karetskas.ArrayListTask
+{
+    internal class Program
+    {
+        private enum PrintType
+        {
+            Write,
+            WriteLine
+        }
+
+        static void Main()
+        {
+            ArrayList<string> additionResult = GetItemsList();
+
+            PrintToConsole(ConsoleColor.DarkYellow, "Adding one element:", $"List before adding: {additionResult}.", PrintType.Write);
+            Console.WriteLine();
+
+            additionResult.Add("456");
+
+            PrintToConsole(ConsoleColor.Yellow, "", $"List after adding: {additionResult}.");
+
+            ArrayList<string> listToSetItem = GetItemsList();
+
+            PrintToConsole(ConsoleColor.DarkYellow, "Changing the fourth element with an indexer.",
+                $"List before change: {listToSetItem}.", PrintType.Write);
+            Console.WriteLine();
+
+            listToSetItem[3] = "000";
+
+            PrintToConsole(ConsoleColor.Yellow, "", $"List after change: {listToSetItem}.");
+
+            ArrayList<string> listToGetElement = GetItemsList();
+
+            PrintToConsole(ConsoleColor.DarkYellow, "Reading the third element with an indexer.",
+                $"List have elements: {listToGetElement}. Third element => {listToGetElement[2]}");
+
+            ArrayList<string> clearingResult = GetItemsList();
+
+            PrintToConsole(ConsoleColor.DarkYellow, "Clearing list:", $"List before clearing: {clearingResult}.", PrintType.Write);
+            Console.WriteLine();
+
+            clearingResult.Clear();
+
+            PrintToConsole(ConsoleColor.Yellow, "", $"List after clearing: {clearingResult}.");
+
+            ArrayList<string> list = GetItemsList();
+
+            PrintToConsole(ConsoleColor.DarkYellow, "Check if an element exists and return its index:", $"Search list: {list}.", PrintType.Write);
+            Console.WriteLine();
+
+            PrintToConsole(ConsoleColor.Yellow, "", $"Does element \"16\" exist? {list.Contains("16")}. Index = {list.IndexOf("16")}.", PrintType.Write);
+            Console.WriteLine();
+
+            PrintToConsole(ConsoleColor.Yellow, "", $"Does element \"99\" exist? {list.Contains("99")}. Index = {list.IndexOf("99")}.");
+
+            ArrayList<string> listToCopying = GetItemsList();
+
+            PrintToConsole(ConsoleColor.DarkYellow, "Copying list to array:", $"List: {listToCopying}", PrintType.Write);
+            Console.WriteLine();
+
+            string[] stringsArray = new string[list.Count];
+
+            list.CopyTo(stringsArray, 2);
+
+            PrintToConsole(ConsoleColor.Yellow, "", $"Array: {string.Join(", ", stringsArray)}.");
+
+            ArrayList<string> listForInsertingEtem = new ArrayList<string>(0);
+
+            PrintToConsole(ConsoleColor.DarkYellow, "Insert item into the list:",
+                $"List before change: {listForInsertingEtem}. Capacity = {listForInsertingEtem.Capacity}; Count = {listForInsertingEtem.Count}.", PrintType.Write);
+            Console.WriteLine();
+
+            for (int i = 0; i < 4; i++)
+            {
+                listForInsertingEtem.Insert(i, Convert.ToString(i + 1));
+            }
+
+            listForInsertingEtem.Insert(3, "5");
+
+            PrintToConsole(ConsoleColor.Yellow, "", $"List after change: {listForInsertingEtem}. " +
+                $"Capacity = {listForInsertingEtem.Capacity}; Count = {listForInsertingEtem.Count}.");
+
+            ArrayList<string> listToRemoveByIndex = GetItemsList();
+
+            PrintToConsole(ConsoleColor.DarkYellow, "Deleting item that has index 3:", $"List before change: {listToRemoveByIndex}. " +
+                $"Capacity = {listToRemoveByIndex.Capacity}; Count = {listToRemoveByIndex.Count}.", PrintType.Write);
+            Console.WriteLine();
+
+            listToRemoveByIndex.RemoveAt(3);
+
+            PrintToConsole(ConsoleColor.Yellow, "", $"List after removal item: {listToRemoveByIndex}. " +
+                $"Capacity = {listToRemoveByIndex.Capacity}; Count = {listToRemoveByIndex.Count}.");
+
+            ArrayList<string> ListToRemoveByItem = GetItemsList();
+
+            PrintToConsole(ConsoleColor.DarkYellow, "Deleting item that has number 16:", $"List before change: {ListToRemoveByItem}. " +
+                $"Capacity = {ListToRemoveByItem.Capacity}; Count = {ListToRemoveByItem.Count}.", PrintType.Write);
+            Console.WriteLine();
+
+            ListToRemoveByItem.Remove("16");
+
+            PrintToConsole(ConsoleColor.Yellow, "", $"List after removal item: {ListToRemoveByItem}. " +
+                $"Capacity = {ListToRemoveByItem.Capacity}; Count = {ListToRemoveByItem.Count}.");
+
+            ArrayList<string> listForTrimExcess = GetItemsList(11);
+
+            PrintToConsole(ConsoleColor.DarkYellow, "Trim excess in the list:",
+                $"List before change: {listForTrimExcess}. Capacity: {listForTrimExcess.Capacity}. Count: {listForTrimExcess.Count}.", PrintType.Write);
+            Console.WriteLine();
+
+            for (int i = 0; i < 2; i++)
+            {
+                listForTrimExcess.RemoveAt(listForTrimExcess.Count - 1);
+            }
+
+            listForTrimExcess.TrimExcess();
+
+            PrintToConsole(ConsoleColor.Yellow, "", $"List after change: {listForTrimExcess}. " +
+                $"Capacity: {listForTrimExcess.Capacity}. Count: {listForTrimExcess.Count}.");
+        }
+
+        private static ArrayList<string> GetItemsList(int itemsCount = 7)
+        {
+            if (itemsCount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(itemsCount), $"The \"{nameof(itemsCount)}\" argument out of range of array. " +
+                    $"Valid range from 0 to {int.MaxValue}.");
+            }
+
+            ArrayList<string> list = new ArrayList<string>(itemsCount);
+
+            for (int i = 0; i < itemsCount; i++)
+            {
+                list.Add(Convert.ToString(i * i));
+            }
+
+            return list;
+        }
+
+        private static void PrintToConsole<T>(ConsoleColor color, string title, T? text, PrintType printType = PrintType.WriteLine)
+        {
+            if (!string.IsNullOrEmpty(title))
+            {
+                Console.WriteLine(title);
+            }
+
+            Console.ForegroundColor = color;
+
+            if (printType == PrintType.Write)
+            {
+                Console.Write(text);
+
+                Console.ResetColor();
+
+                return;
+            }
+
+            Console.WriteLine(text);
+
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(new string('-', 90));
+            Console.ResetColor();
+        }
+    }
+}
