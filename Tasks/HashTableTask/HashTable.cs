dd77558425d5c2044129d3532884581d9b19686e
Karetskas_Evgeny <kub99999@yandex.ru>
2022-09-21 20:36:30 +0500

    Created "HashTableTask" project.

---
+++ Tasks/HashTableTask/HashTable.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Academits.Karetskas.HashTableTask
+{
+    public sealed class HashTable<T> : ICollection<T>
+    {
+        private readonly List<T>[] _table;
+        private int modCount;
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => false;
+
+        public HashTable(int capacity)
+        {
+            if (capacity <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(capacity), $"Argument out of range of table. Valid range is from 1 to {int.MaxValue}.");
+            }
+
+            _table = new List<T>[capacity];
+        }
+
+        public void Add(T item)
+        {
+            int index = GetIndex(item);
+
+            if (_table[index] is not null)
+            {
+                _table[index].Add(item);
+            }
+            else
+            {
+                _table[index] = new List<T>(1);
+
+                _table[index].Add(item);
+            }
+
+            Count++;
+            modCount++;
+        }
+
+        private int GetIndex(T item)
+        {
+            if (item is null)
+            {
+                return 0;
+            }
+
+            return Math.Abs(item.GetHashCode() % _table.Length);
+        }
+
+        public void Clear()
+        {
+            Array.Clear(_table);
+
+            Count = 0;
+            modCount++;
+        }
+
+        public bool Contains(T item)
+        {
+            int index = GetIndex(item);
+
+            if (_table[index] is null || _table[index].Count == 0)
+            {
+                return false;
+            }
+
+            return _table[index].Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array), $"Argument \"{nameof(array)}\" is null.");
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("The length of array must be greate than 0.", nameof(array));
+            }
+
+            if (arrayIndex < 0 || arrayIndex >= array.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex), $"The \"{nameof(arrayIndex)}\" out of range of array. "
+                    + $"Valid range from 0 to {array.Length - 1}");
+            }
+
+            if (array.Length - (arrayIndex + 1) > Count)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(array)} and {nameof(arrayIndex)}",
+                    $"With the current parameters of the array length = {array.Length} and "
+                    + $"array start index = {arrayIndex}, it is impossible to copy all the elements of the hash table. "
+                    + $"Valid range from {Count} and {int.MaxValue}.");
+            }
+
+            int index = arrayIndex;
+
+            foreach (T item in this)
+            {
+                array[index] = item;
+
+                index++;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            int index = GetIndex(item);
+
+            if (_table[index] is null || _table[index].Count == 0)
+            {
+                return false;
+            }
+
+            bool isRemovedItem = _table[index].Remove(item);
+
+            if (isRemovedItem)
+            {
+                Count--;
+                modCount++;
+            }
+
+            return isRemovedItem;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            int currentModCount = modCount;
+
+            for (int i = 0; i < _table.Length; i++)
+            {
+                if (currentModCount != modCount)
+                {
+                    throw new InvalidOperationException("The hash table has been modified during the iteration.");
+                }
+
+                if (_table[i] is null || _table[i].Count == 0)
+                {
+                    continue;
+                }
+
+                for (int j = 0; j < _table[i].Count; j++)
+                {
+                    yield return _table[i][j];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append('[');
+
+            for (int i = 0; i < _table.Length; i++)
+            {
+                if (_table[i] is null || _table[i].Count == 0)
+                {
+                    stringBuilder.Append("NULL")
+                        .Append(", ");
+
+                    continue;
+                }
+
+                stringBuilder.Append('[');
+
+                for (int j = 0; j < _table[i].Count; j++)
+                {
+                    if (_table[i][j] is null)
+                    {
+                        stringBuilder.Append("NULL")
+                            .Append(", ");
+
+                        continue;
+                    }
+
+                    stringBuilder.Append(_table[i][j])
+                        .Append(", ");
+                }
+
+                stringBuilder.Remove(stringBuilder.Length - 2, 2)
+                    .Append(']')
+                    .Append(", ");
+            }
+
+            return stringBuilder.Remove(stringBuilder.Length - 2, 2)
+                .Append(']')
+                .ToString();
+        }
+    }
+}
---
+++ Tasks/HashTableTask/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Academits.Karetskas.HashTableTask
+{
+    internal class Program
+    {
+        enum PrintType
+        {
+            Write = 0,
+            WriteLine = 1
+        }
+
+        static void Main()
+        {
+            HashTable<string> hashTableToAddingItems = new HashTable<string>(10);
+
+            hashTableToAddingItems.Add("11");
+            hashTableToAddingItems.Add("22");
+            hashTableToAddingItems.Add("33");
+            hashTableToAddingItems.Add("44");
+            hashTableToAddingItems.Add("55");
+            hashTableToAddingItems.Add("66");
+            hashTableToAddingItems.Add("77");
+            hashTableToAddingItems.Add("88");
+            hashTableToAddingItems.Add("99");
+            hashTableToAddingItems.Add("100");
+
+            PrintToConsole(ConsoleColor.DarkYellow, "Adding items to the hash table:", $"Hash table: {hashTableToAddingItems}.", PrintType.Write);
+            Console.WriteLine();
+
+            PrintToConsole(ConsoleColor.Yellow, "", $"Hash table has {hashTableToAddingItems.Count} items.");
+
+            HashTable<string> hashTableForClearing = new HashTable<string>(3);
+
+            hashTableForClearing.Add("Hello");
+            hashTableForClearing.Add("world");
+            hashTableForClearing.Add("!");
+
+            PrintToConsole(ConsoleColor.DarkYellow, "Clearing items for the hash table:",
+                $"Hash table before clearing: {hashTableForClearing}. Count items: {hashTableForClearing.Count}", PrintType.Write);
+            Console.WriteLine();
+
+            hashTableForClearing.Clear();
+
+            PrintToConsole(ConsoleColor.Yellow, "", $"Hash table after clearing: {hashTableForClearing}. Count items: {hashTableForClearing.Count}.");
+
+            HashTable<int> hashTableForSearch = new HashTable<int>(3);
+
+            hashTableForSearch.Add(444);
+            hashTableForSearch.Add(555);
+            hashTableForSearch.Add(666);
+
+            PrintToConsole(ConsoleColor.DarkYellow, "Check whether the hash table contains the numbers \"555\" and \"999\".",
+                $"Number \"555\" - {hashTableForSearch.Contains(555)}. Number \"999\" - {hashTableForSearch.Contains(999)}.");
+
+            HashTable<string> HashTableToArray = new HashTable<string>(3);
+
+            HashTableToArray.Add("H4el");
+            HashTableToArray.Add("wo3r8");
+            HashTableToArray.Add("!!!!!");
+
+            PrintToConsole(ConsoleColor.DarkYellow, "Copying hash table to array:", $"Hash table: {HashTableToArray}.", PrintType.Write);
+            Console.WriteLine();
+
+            string[] array = new string[HashTableToArray.Count];
+
+            HashTableToArray.CopyTo(array, 0);
+
+            PrintToConsole(ConsoleColor.Yellow, "", $"Array: [{string.Join(", ", array)}].");
+
+            HashTable<int> hashTableForDeletingItems = new HashTable<int>(4);
+
+            hashTableForDeletingItems.Add(111);
+            hashTableForDeletingItems.Add(222);
+            hashTableForDeletingItems.Add(333);
+            hashTableForDeletingItems.Add(444);
+
+            PrintToConsole(ConsoleColor.DarkYellow, "Deleting items for hash table:", $"Hash table before deleting items: {hashTableForDeletingItems}.", PrintType.Write);
+            Console.WriteLine();
+
+            hashTableForDeletingItems.Remove(222);
+            hashTableForDeletingItems.Remove(333);
+            hashTableForDeletingItems.Remove(111);
+
+            PrintToConsole(ConsoleColor.Yellow, "", $"Hash table after deleting items: {hashTableForDeletingItems}.");
+        }
+
+        private static void PrintToConsole<T>(ConsoleColor color, string title, T? text, PrintType printType = PrintType.WriteLine)
+        {
+            if (!string.IsNullOrEmpty(title))
+            {
+                Console.WriteLine(title);
+            }
+
+            Console.ForegroundColor = color;
+
+            if (printType == PrintType.Write)
+            {
+                Console.Write(text);
+
+                Console.ResetColor();
+
+                return;
+            }
+
+            Console.WriteLine(text);
+
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(new string('-', 90));
+            Console.ResetColor();
+        }
+    }
+}
