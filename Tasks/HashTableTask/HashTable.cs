628028c3296d3178053593ea40a57574b64ecb9d
Karetskas_Evgeny <kub99999@yandex.ru>
2022-09-30 21:09:32 +0500

    Fixed bugs in the "HashTable" class of the "HashTableTask" project.
    1. Added constant: "_initialCapacity";
    2. Renamed field:
    "_table" to "_listsArray",
    "modCount" to "_modCount".
    3. The "HashTable()" constructor has been created.
    4. The "Add" function has been refactored.
    5. Added condition to the "Clear" function.
    6. The "Contains" function has been refactored.
    7. Changed error handling conditions.
    8. The "isRemovedItem" variable of the "Remove" function has been renamed to "isItemRemoved".
    9. The "GetEnumerator" function:
    - The "currentModCount" variable has been renamed to "initialModCount".
    - The error handling condition has been moved to another loop.
    10. Changed loops and logic in "ToString" function.

--- Tasks/HashTableTask/HashTable.cs
+++ Tasks/HashTableTask/HashTable.cs
@@ -7,40 +7,42 @@ namespace Academits.Karetskas.HashTableTask
 {
     public sealed class HashTable<T> : ICollection<T>
     {
-        private readonly List<T>[] _table;
-        private int modCount;
+        private const int _initialCapacity = 10;
+        private readonly List<T>?[] _listsArray;
+        private int _modCount;
 
         public int Count { get; private set; }
 
         public bool IsReadOnly => false;
 
+        public HashTable()
+        {
+            _listsArray = new List<T>[_initialCapacity];
+        }
+
         public HashTable(int capacity)
         {
             if (capacity <= 0)
             {
-                throw new ArgumentOutOfRangeException(nameof(capacity), $"Argument out of range of table. Valid range is from 1 to {int.MaxValue}.");
+                throw new ArgumentOutOfRangeException(nameof(capacity), $"Argument out of range of table. Valid range is greater than or equal to 1.");
             }
 
-            _table = new List<T>[capacity];
+            _listsArray = new List<T>[capacity];
         }
 
         public void Add(T item)
         {
             int index = GetIndex(item);
 
-            if (_table[index] is not null)
+            if (_listsArray[index] is null)
             {
-                _table[index].Add(item);
+                _listsArray[index] = new List<T>();
             }
-            else
-            {
-                _table[index] = new List<T>(1);
 
-                _table[index].Add(item);
-            }
+            _listsArray[index]!.Add(item);
 
             Count++;
-            modCount++;
+            _modCount++;
         }
 
         private int GetIndex(T item)
@@ -50,27 +52,27 @@ namespace Academits.Karetskas.HashTableTask
                 return 0;
             }
 
-            return Math.Abs(item.GetHashCode() % _table.Length);
+            return Math.Abs(item.GetHashCode() % _listsArray.Length);
         }
 
         public void Clear()
         {
-            Array.Clear(_table);
+            if (Count == 0)
+            {
+                return;
+            }
+
+            Array.Clear(_listsArray);
 
             Count = 0;
-            modCount++;
+            _modCount++;
         }
 
         public bool Contains(T item)
         {
             int index = GetIndex(item);
 
-            if (_table[index] is null || _table[index].Count == 0)
-            {
-                return false;
-            }
-
-            return _table[index].Contains(item);
+            return _listsArray[index] is null ? false : _listsArray[index]!.Contains(item);
         }
 
         public void CopyTo(T[] array, int arrayIndex)
@@ -80,23 +82,16 @@ namespace Academits.Karetskas.HashTableTask
                 throw new ArgumentNullException(nameof(array), $"Argument \"{nameof(array)}\" is null.");
             }
 
-            if (array.Length == 0)
-            {
-                throw new ArgumentException("The length of array must be greate than 0.", nameof(array));
-            }
-
-            if (arrayIndex < 0 || arrayIndex >= array.Length)
+            if (arrayIndex < 0)
             {
                 throw new ArgumentOutOfRangeException(nameof(arrayIndex), $"The \"{nameof(arrayIndex)}\" out of range of array. "
-                    + $"Valid range from 0 to {array.Length - 1}");
+                    + "Valid range is greater than or equal to 0.");
             }
 
-            if (array.Length - (arrayIndex + 1) > Count)
+            if (array.Length - arrayIndex < Count)
             {
-                throw new ArgumentOutOfRangeException($"{nameof(array)} and {nameof(arrayIndex)}",
-                    $"With the current parameters of the array length = {array.Length} and "
-                    + $"array start index = {arrayIndex}, it is impossible to copy all the elements of the hash table. "
-                    + $"Valid range from {Count} and {int.MaxValue}.");
+                throw new ArgumentException("The number of elements in the HashTable is greater than the available space from arrayindex to"
+                    + $"the end of the destination array.Valid range is greater than or equal to {Count}.", $"{nameof(array)}, {nameof(arrayIndex)}");
             }
 
             int index = arrayIndex;
@@ -113,41 +108,41 @@ namespace Academits.Karetskas.HashTableTask
         {
             int index = GetIndex(item);
 
-            if (_table[index] is null || _table[index].Count == 0)
+            if (_listsArray[index] is null || _listsArray[index]!.Count == 0)
             {
                 return false;
             }
 
-            bool isRemovedItem = _table[index].Remove(item);
+            bool isItemRemoved = _listsArray[index]!.Remove(item);
 
-            if (isRemovedItem)
+            if (isItemRemoved)
             {
                 Count--;
-                modCount++;
+                _modCount++;
             }
 
-            return isRemovedItem;
+            return isItemRemoved;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            int currentModCount = modCount;
+            int initialModCount = _modCount;
 
-            for (int i = 0; i < _table.Length; i++)
+            foreach (List<T>? list in _listsArray)
             {
-                if (currentModCount != modCount)
-                {
-                    throw new InvalidOperationException("The hash table has been modified during the iteration.");
-                }
-
-                if (_table[i] is null || _table[i].Count == 0)
+                if (list is null)
                 {
                     continue;
                 }
 
-                for (int j = 0; j < _table[i].Count; j++)
+                foreach (T item in list)
                 {
-                    yield return _table[i][j];
+                    if (initialModCount != _modCount)
+                    {
+                        throw new InvalidOperationException("The hash table has been modified during the iteration.");
+                    }
+
+                    yield return item;
                 }
             }
         }
@@ -163,35 +158,32 @@ namespace Academits.Karetskas.HashTableTask
 
             stringBuilder.Append('[');
 
-            for (int i = 0; i < _table.Length; i++)
+            foreach (List<T>? list in _listsArray)
             {
-                if (_table[i] is null || _table[i].Count == 0)
+                if (list is null || list.Count == 0)
                 {
-                    stringBuilder.Append("NULL")
-                        .Append(", ");
+                    stringBuilder.Append("NULL, ");
 
                     continue;
                 }
 
                 stringBuilder.Append('[');
 
-                for (int j = 0; j < _table[i].Count; j++)
+                foreach (T item in list)
                 {
-                    if (_table[i][j] is null)
+                    if (item is null)
                     {
-                        stringBuilder.Append("NULL")
-                            .Append(", ");
+                        stringBuilder.Append("NULL, ");
 
                         continue;
                     }
 
-                    stringBuilder.Append(_table[i][j])
+                    stringBuilder.Append(item)
                         .Append(", ");
                 }
 
                 stringBuilder.Remove(stringBuilder.Length - 2, 2)
-                    .Append(']')
-                    .Append(", ");
+                        .Append("], ");
             }
 
             return stringBuilder.Remove(stringBuilder.Length - 2, 2)
